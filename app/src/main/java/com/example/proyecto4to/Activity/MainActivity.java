6e79971f844ca8c4f06949062f717f224c3eb336
Geasy01 <gaelemm1@gmail.com>
2022-12-08 13:17:43 -0600

    recordarme

--- app/src/main/java/com/example/proyecto4to/Activity/MainActivity.java
+++ app/src/main/java/com/example/proyecto4to/Activity/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.proyecto4to.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.proyecto4to.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.UUID;
 
@@ -16,11 +20,32 @@ public class MainActivity extends AppCompatActivity {
     public final static int MESSAGE_READ = 2;
     private final static int CONNECTING_STATUS = 3;
 
+    BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomnavigationbar);
+        bottomNavigationView.setBackground(null);
+        bottomNavigationView.getMenu().getItem(2).setEnabled(false);
+        getSupportFragmentManager().beginTransaction().replace(R.id.framecontainer,new FragmentInicio()).commit();
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment temp = null;
+                switch (item.getItemId())
+                {
+                    case R.id.mHome : temp = new FragmentInicio();
+                        break;
+                    case R.id.mPerson : temp = new FragmentPerfil();
+                        break;
+                    case R.id.mSetting : temp = new FragmentConfig();
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.framecontainer,temp).commit();
+                return true;
+            }
+        });
     }
 }
