c1c3a6cf784463dbf9ef6b150a38fe8c0d52ab37
Geasy01 <gaelemm1@gmail.com>
2022-12-09 23:41:27 -0600

    nav bar

--- app/src/main/java/com/example/proyecto4to/Activity/MainActivity.java
+++ app/src/main/java/com/example/proyecto4to/Activity/MainActivity.java
@@ -3,41 +3,50 @@ package com.example.proyecto4to.Activity;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.example.proyecto4to.R;
+import com.example.proyecto4to.databinding.ActivityMainBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
-    BottomNavigationView bottomNavigationView;
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new FragmentInicio());
 
-        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomnavigationView);
-        bottomNavigationView.setBackground(null);
-        bottomNavigationView.getMenu().getItem(2).setEnabled(false);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment temp = null;
-                switch (item.getItemId())
-                {
-                    case R.id.mHome : temp = new FragmentInicio();
-                        break;
-                    case R.id.mPerson : temp = new FragmentPerfil();
-                        break;
-                    case R.id.mSetting : temp = new FragmentConfig();
-                }
-                return true;
+        binding.bottomnavigationView.setOnItemSelectedListener(item ->{
+            switch (item.getItemId()){
+                case R.id.mHome:
+                    replaceFragment(new FragmentInicio());
+                    break;
+                case R.id.mPerson:
+                    replaceFragment(new FragmentPerfil());
+                    break;
+                case R.id.mSetting:
+                    replaceFragment(new FragmentConfig());
+                    break;
             }
+
+            return true;
         });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.framelayout, fragment);
+        fragmentTransaction.commit();
     }
 }
