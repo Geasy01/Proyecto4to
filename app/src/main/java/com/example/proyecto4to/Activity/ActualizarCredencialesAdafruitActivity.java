0a7644a15aa87987c6499b9904a37d1024ec8cd5
Geasy01 <gaelemm1@gmail.com>
2022-12-12 08:57:09 -0600

    boton feed

---
+++ app/src/main/java/com/example/proyecto4to/Activity/ActualizarCredencialesAdafruitActivity.java
@@ -0,0 +1,16 @@
+package com.example.proyecto4to.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.proyecto4to.R;
+
+public class ActualizarCredencialesAdafruitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actualizar_credenciales_adafruit);
+    }
+}
