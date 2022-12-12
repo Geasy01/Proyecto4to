f3d49bbbd08cc5bef5afdd14b8a93c4c85bea0c0
arkadiem <jasonusotelo@gmail.com>
2022-12-12 13:40:01 -0600

    Grapics

---
+++ app/src/main/java/com/example/proyecto4to/Modelos/DistanciaData.java
@@ -0,0 +1,14 @@
+package com.example.proyecto4to.Modelos;
+
+public class DistanciaData {
+
+    private final int value;
+
+    public DistanciaData(int value){
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
